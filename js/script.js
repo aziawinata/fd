4a0483b4acf9a6294a3459f84dc40c9adcd56d14
aziawinata <47654843+aziawinata@users.noreply.github.com>
2019-02-15 14:46:41 +0700

    obras

---
+++ js/script.js
@@ -0,0 +1,39 @@
+$('.page-scroll').on('click', function(e) {
+
+ var tujuan = $(this).attr('href');
+
+ var elemenTujuan = $(tujuan);
+
+ $('html , body').animate({
+  scrollTop: elemenTujuan.offset().top - 50
+ }, 1000, 'swing');
+
+ e.preventDefault();
+});﻿
+
+
+$(window).scroll(function(){
+	var wScroll = $(this).scrollTop();
+
+	$('.jumbotron img').css({
+		'transform': 'translate(0px, '+ wScroll/4 +'%)'
+
+	});
+
+	$('.jumbotron h1').css({
+		'transform': 'translate(0px, '+ wScroll/2 +'%)'
+
+	});
+
+	$('.jumbotron p').css({
+		'transform': 'translate(0px, '+ wScroll/1.2 +'%)'
+});
+
+	
+
+});
+
+
+
+
+
